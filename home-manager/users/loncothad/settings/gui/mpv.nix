9f5315b1d129e4f335dcd1b3a741c1c8d7fa0008
loncothad <loncothad@gmail.com>
2026-07-08 00:37:04 +0300

    fix mpv mod

--- home-manager/users/loncothad/settings/gui/mpv.nix
+++ home-manager/users/loncothad/settings/gui/mpv.nix
@@ -5,6 +5,7 @@
     enable = true;
     xdgIntegration.enable = true;
 
+    # mpv.conf key-value overrides
     config = {
       vo = "gpu";
       gpu-api = "vulkan"; # FIXME: make it optional depending on hardware configuration?
@@ -22,14 +23,14 @@
       sub-border-size = 2;
       sub-color = "1.0/1.0/1.0/1.0";
       sub-border-color = "0.0/0.0/0.0/1.0";
-
-      scripts = with pkgs.mpvScripts; [
-        mpris
-        uosc
-        thumbfast
-        visualizer
-        webtorrent-mpv-hook
-      ];
     };
+
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      uosc
+      thumbfast
+      visualizer
+      webtorrent-mpv-hook
+    ];
   };
 }
